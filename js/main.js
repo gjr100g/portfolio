10630f585fb250e3a4c3511ba8c57ad7a1c45fa1
gjr100g <93234776+gjr100g@users.noreply.github.com>
2022-03-28 15:58:15 +0900

    Revert "03-28"
    This reverts commit 2d990f6f9bef24782406744160133ecca3f40a02.

--- js/main.js
+++ js/main.js
@@ -61,11 +61,6 @@
                 }else{
                     $('nav li:nth-of-type(7)').removeClass("licolor");
                 }
-                if ($(window).scrollTop() == popo*7) {
-                    $('nav li:nth-of-type(8)').addClass("licolor");
-                }else{
-                    $('nav li:nth-of-type(8)').removeClass("licolor");
-                }
              });
              
              $(function(){
@@ -106,7 +101,7 @@
         $(window).on("wheel", function (e) {
             if (mHtml.is(":animated")) return;
             if (e.originalEvent.deltaY > 0) {
-                if (page == 8) return;
+                if (page == 7) return;
                 page++;
             } else if (e.originalEvent.deltaY < 0) {
                 if (page == 1) return;
@@ -121,37 +116,37 @@
         $(function () {
             // 포트폴리오 1 pc
             $('.portfolio1 .portfolio_photo').mouseover(function () {
-                $('.portfolio1 .portfolio_photo img').stop().animate({ 'marginTop': '-1827.83px' }, 5000);
+                $('.portfolio1 .portfolio_photo img').stop().animate({ 'marginTop': '-2243.69px' }, 5000);
             });
             $('.portfolio1 .portfolio_photo').mouseout(function () {
                 $('.portfolio1 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
             });
-                    // 포트폴리오 1 모바일
-                    $('.portfolio1 .mobile').mouseover(function () {
-                        $('.portfolio1 .img2 img').stop().animate({ 'marginTop': '-1780px' }, 5000);
-                    });
-                    $('.portfolio1 .mobile').mouseout(function () {
-                        $('.portfolio1 .img2 img').stop().animate({ 'marginTop': '0px' }, 2000);
-                    });
 
             // 포트폴리오 2 pc
             $('.portfolio2 .portfolio_photo').mouseover(function () {
-                $('.portfolio2 .portfolio_photo img').stop().animate({ 'marginTop': '-2243.69px' }, 5000);
+                $('.portfolio2 .portfolio_photo img').stop().animate({ 'marginTop': '-1827.83px' }, 5000);
             });
             $('.portfolio2 .portfolio_photo').mouseout(function () {
                 $('.portfolio2 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
             });
+                    // 포트폴리오 2 모바일
+                    $('.portfolio2 .mobile').mouseover(function () {
+                        $('.portfolio2 .img2 img').stop().animate({ 'marginTop': '-1780px' }, 5000);
+                    });
+                    $('.portfolio2 .mobile').mouseout(function () {
+                        $('.portfolio2 .img2 img').stop().animate({ 'marginTop': '0px' }, 2000);
+                    });
 
             // 포트폴리오 3 pc
             $('.portfolio3 .portfolio_photo').mouseover(function () {
-                $('.portfolio3 .portfolio_photo img').stop().animate({ 'marginTop': '-1827.83px' }, 5000);
+                $('.portfolio3 .portfolio_photo img').stop().animate({ 'marginTop': '-2087.84px' }, 5000);
             });
             $('.portfolio3 .portfolio_photo').mouseout(function () {
                 $('.portfolio3 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
             });
                     // 포트폴리오 3 모바일
                     $('.portfolio3 .mobile').mouseover(function () {
-                        $('.portfolio3 .img2 img').stop().animate({ 'marginTop': '-1780px' }, 5000);
+                        $('.portfolio3 .img2 img').stop().animate({ 'marginTop': '-2144px' }, 5000);
                     });
                     $('.portfolio3 .mobile').mouseout(function () {
                         $('.portfolio3 .img2 img').stop().animate({ 'marginTop': '0px' }, 2000);
@@ -159,14 +154,14 @@
 
             // 포트폴리오 4 pc
             $('.portfolio4 .portfolio_photo').mouseover(function () {
-                $('.portfolio4 .portfolio_photo img').stop().animate({ 'marginTop': '-2087.84px' }, 5000);
+                $('.portfolio4 .portfolio_photo img').stop().animate({ 'marginTop': '-2157.64px' }, 5000);
             });
             $('.portfolio4 .portfolio_photo').mouseout(function () {
                 $('.portfolio4 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
             });
                     // 포트폴리오 4 모바일
                     $('.portfolio4 .mobile').mouseover(function () {
-                        $('.portfolio4 .img2 img').stop().animate({ 'marginTop': '-2144px' }, 5000);
+                        $('.portfolio4 .img2 img').stop().animate({ 'marginTop': '-2394px' }, 5000);
                     });
                     $('.portfolio4 .mobile').mouseout(function () {
                         $('.portfolio4 .img2 img').stop().animate({ 'marginTop': '0px' }, 2000);
@@ -174,24 +169,9 @@
 
             // 포트폴리오 5 pc
             $('.portfolio5 .portfolio_photo').mouseover(function () {
-                $('.portfolio5 .portfolio_photo img').stop().animate({ 'marginTop': '-2157.64px' }, 5000);
+                $('.portfolio5 .portfolio_photo img').stop().animate({ 'marginTop': '-2897.52px' }, 5000);
             });
             $('.portfolio5 .portfolio_photo').mouseout(function () {
                 $('.portfolio5 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
-            });
-                    // 포트폴리오 5 모바일
-                    $('.portfolio5 .mobile').mouseover(function () {
-                        $('.portfolio5 .img2 img').stop().animate({ 'marginTop': '-2394px' }, 5000);
-                    });
-                    $('.portfolio5 .mobile').mouseout(function () {
-                        $('.portfolio5 .img2 img').stop().animate({ 'marginTop': '0px' }, 2000);
-                    });
-
-            // 포트폴리오 6 pc
-            $('.portfolio6 .portfolio_photo').mouseover(function () {
-                $('.portfolio6 .portfolio_photo img').stop().animate({ 'marginTop': '-2897.52px' }, 5000);
-            });
-            $('.portfolio6 .portfolio_photo').mouseout(function () {
-                $('.portfolio6 .portfolio_photo img').stop().animate({ 'marginTop': '0px' }, 2000);
             });
         });
